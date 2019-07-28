f5466bfe5d374fe983a182752e2399b9986607eb
708817 <aqwtraitor@yahoo.com>
2019-07-28 19:43:32 +0800

    v0.5a

--- app/src/main/java/com/example/tmt_mobile/AsyncResponse.java
+++ app/src/main/java/com/example/tmt_mobile/AsyncResponse.java
@@ -1,3 +1,10 @@
+/*
+DESCRIPTION OF AsyncResponse.java
+        Interface ito para makuha ng activities na nirereturn ng WS classes. Since outer class
+        yung mga AsyncTasks, imposibleng kunin yung nirereturn nila unless may interface na
+        mababasehan.
+*/
+
 package com.example.tmt_mobile;
 
 public interface AsyncResponse {
